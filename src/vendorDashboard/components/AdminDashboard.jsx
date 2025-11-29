c59693eef719e9a5ee67a98bf4d91cfb537c6677
SivaSai <sivasaip86@gmail.com>
2025-11-29 07:47:09 +0530

    Fix import path for apiPath in AdminDashboard

--- src/vendorDashboard/components/AdminDashboard.jsx
+++ src/vendorDashboard/components/AdminDashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { API_Path } from "../../data/apiPath";
+import { API_Path } from "../data/apiPath";
 import "../styles/adminDashboard.css";
 
 const AdminDashboard = () => {
